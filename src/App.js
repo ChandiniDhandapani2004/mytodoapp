960719f12acad3fef20bbd8dfd980b7a694374f9
ChandiniDhandapani2004 <142870141+ChandiniDhandapani2004@users.noreply.github.com>
2024-08-04 22:54:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import TodoList from './components/TodoList';
+
+/**
+ * Main application component.
+ */
+function App() {
+  return (
+    <Router>
+      <div className="todo-app">
+        <Routes>
+          <Route path="/" element={<TodoList />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
 
